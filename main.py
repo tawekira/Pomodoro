366926d0311cc0140fa81ee50e6a1e14104d4f9f
Rayn <rayvialtan@gmail.com>
2024-04-28 09:26:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,153 @@
+from tkinter import *
+import math
+
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text = "Pomodoro\nTimer")
+    tick_label.config(text = "")
+    start_button.config(state = NORMAL)
+    global reps
+    reps = 0
+
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps +=1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    start_button.config(state = DISABLED)
+
+    if reps % 8 ==0:
+        count_down(long_break_sec)
+        title_label.config(text = "Break", fg = RED)
+    elif reps % 2 ==0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    count_sec = count%60
+    if count_sec <10:
+        count_sec = f"0{count_sec}"
+
+
+
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+
+    if count>0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+
+    if reps%2 == 0:
+        num = int(reps/2)
+        string = ""
+        for i in range(num):
+            string+="✔"
+        tick_label.config(text = f"{string}", fg=GREEN)
+
+
+# ---------------------------- POPUP WINDOW ------------------------------- #
+def open_popup():
+    popup = Toplevel(window)
+    popup.title("")
+    popup.config(padx = 50, pady=50, bg = YELLOW)
+
+    label = Label(popup, text = "Pomodoro Timer", font=(FONT_NAME, 15), bg = YELLOW)
+    label.grid(column =0, row = 0)
+
+    text = ("The Pomodoro Technique is a time management method\n"
+            "to boost productivity and improve study habits. It\n"
+            "divides your work and breaks into regular, short\n"
+            "increments so that you avoid feeling overwhelmed and\n"
+            "burned out.\n"
+            "\n"
+            "◯ Work 25 Minutes      \n"
+            "◯ Short Break 5 Minutes\n"
+            "◯ Work 25 Minutes      \n"
+            "◯ Short Break 5 Minutes\n"
+            "◯ Work 25 Minutes      \n"
+            "◯ Short Break 5 Minutes\n"
+            "◯ Work 25 Minutes      \n"
+            "◯ Long Break 20 Minutes\n"
+            "\n"
+            "1 ✔ for 1 work session"
+            )
+
+
+    main = Label(popup,text = text, font = (FONT_NAME, 10), bg= YELLOW)
+    main.grid(column = 0, row = 1)
+
+
+
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady= 50, bg= YELLOW )
+
+
+
+canvas = Canvas(width= 200, height = 224, bg = YELLOW, highlightthickness= 0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image = tomato_img )
+timer_text = canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 25, "bold"))
+canvas.grid(column = 1, row = 1)
+
+
+
+
+title_label = Label(text = "Pomodoro\nTimer", font = (FONT_NAME, 50, "bold"), fg = GREEN, bg = YELLOW)
+title_label.grid(column = 1, row = 0)
+
+start_button = Button(text= "Start", highlightthickness= 0, command= start_timer)
+start_button.grid(column = 0, row = 2)
+
+reset_button = Button(text = "Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column = 2, row = 2)
+
+tick_label = Label(text = "", fg = GREEN, bg = YELLOW)
+tick_label.grid(column = 1, row = 3)
+
+information_button = Button(text = "ⓘ", font = (FONT_NAME, 10), highlightbackground= YELLOW,highlightthickness=0,highlightcolor=YELLOW, bg=YELLOW, command = open_popup)
+information_button.grid(column = 2, row = 0)
+
+
+
+
+
+
+
+window.mainloop()
